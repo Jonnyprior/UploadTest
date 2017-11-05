e23cdb9cc9dcefdb29baec96de7449f50711d3ad
Jonny-PC\Jonny <jonnyprior@hotmail.com>
2017-11-05 22:45:39 +0000

    Added to the right project...whoops

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+print('hello world')
+
+print("how long have you existed?")
+
+print("whats your age?")
+age= int(input("age:"))
+
+days = age * 365.25
+minutes = age * 525948
+seconds =  age * 31556926
+
+print("alive for", days, "days", minutes, "minute and ", seconds, "seconds")
